7aaf2d4be0815be05f2a50ee72c8c77d37d95b5e
Sergio García <98185143+sergigavi@users.noreply.github.com>
2022-07-14 14:32:36 +0200

    creo el proyecto

---
+++ DnD_sgv_ecb_dam/src/main/java/es/sgv/DAM/DnDSgvEcbDamApplication.java
@@ -0,0 +1,13 @@
+package es.sgv.DAM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DnDSgvEcbDamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DnDSgvEcbDamApplication.class, args);
+	}
+
+}
---
+++ DnD_sgv_ecb_dam/src/test/java/es/sgv/DAM/DnDSgvEcbDamApplicationTests.java
@@ -0,0 +1,13 @@
+package es.sgv.DAM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DnDSgvEcbDamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
