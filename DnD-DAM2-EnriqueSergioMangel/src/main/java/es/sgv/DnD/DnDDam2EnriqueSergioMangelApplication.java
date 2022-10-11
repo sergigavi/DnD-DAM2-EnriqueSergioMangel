8b86a5aa2c24e009d5333c45556f0086da8c7138
Sergio García <98185143+sergigavi@users.noreply.github.com>
2022-10-11 13:50:02 +0200

    *

---
+++ DnD-DAM2-EnriqueSergioMangel/src/main/java/es/sgv/DnD/DnDDam2EnriqueSergioMangelApplication.java
@@ -0,0 +1,13 @@
+package es.sgv.DnD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DnDDam2EnriqueSergioMangelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DnDDam2EnriqueSergioMangelApplication.class, args);
+	}
+
+}
