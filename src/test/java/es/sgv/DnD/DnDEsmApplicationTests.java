a5ca6ea0823354541829cb13daa96bddcfb9bd2d
Sergio García <98185143+sergigavi@users.noreply.github.com>
2022-10-11 13:40:07 +0200

    *

---
+++ src/test/java/es/sgv/DnD/DnDEsmApplicationTests.java
@@ -0,0 +1,13 @@
+package es.sgv.DnD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DnDEsmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
