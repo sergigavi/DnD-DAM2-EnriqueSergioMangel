c40833d93f5a5f7f59a43428bff76fc10dbdd6a0
Sergio García <98185143+sergigavi@users.noreply.github.com>
2022-10-26 20:28:49 +0200

    Add files via upload

---
+++ DnD-DAM2-EnriqueSergioMangel/src/main/java/esm/dnd/DnDDAM2EnriqueSergioMangel/modelo/Clase.java
@@ -0,0 +1,6 @@
+package esm.dnd.DnDDAM2EnriqueSergioMangel.modelo;
+
+public enum Clase {
+
+	BARBARO, BARDO, BRUJO, CLERIGO, DRUIDA, EXPLORADOR, GUERRERO, HECHICERO, MAGO, MONJE, PALADIN, PICARO;
+}
---
+++ DnD-DAM2-EnriqueSergioMangel/src/main/java/esm/dnd/DnDDAM2EnriqueSergioMangel/modelo/Equipamiento.java
@@ -0,0 +1,48 @@
+package esm.dnd.DnDDAM2EnriqueSergioMangel.modelo;
+
+import org.springframework.data.annotation.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Builder
+
+public class Equipamiento {
+
+	@Id
+	@NonNull
+	@EqualsAndHashCode.Include
+	private String idEquipo;
+	
+	private String nombre;
+	
+	private TipoEquipo tipo;
+	
+	private CatEquipo categoria;
+	
+	private PropiedadEquipo propiedad;
+	
+	private String modificador;
+	
+	private double danio;
+	
+	private double alcance;
+	
+	private double precio;
+	
+	private double peso;
+	
+	private String ca;
+	
+	private String descripcion;
+	
+	
+}
