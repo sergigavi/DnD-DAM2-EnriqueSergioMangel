4a2bcd64e7cd5d851185c6224c0353b85ea368c2
Sergio García <98185143+sergigavi@users.noreply.github.com>
2022-10-28 10:20:25 +0200

    Update HabilidadController.java

--- DnD-DAM2-EnriqueSergioMangel/src/main/java/esm/dnd/DnDDAM2EnriqueSergioMangel/RESTControllers/HabilidadController.java
+++ DnD-DAM2-EnriqueSergioMangel/src/main/java/esm/dnd/DnDDAM2EnriqueSergioMangel/RESTControllers/HabilidadController.java
@@ -5,7 +5,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import esm.dnd.DnDDAM2EnriqueSergioMangel.modelo.Habilidad;
@@ -27,6 +29,34 @@ public class HabilidadController {
         Iterable<Habilidad> allHabilidades = habilidadServicio.findAllHabilidades();
         
         res = new ResponseEntity<Iterable<Habilidad>>(allHabilidades, HttpStatus.OK);
+        
+        return res;
+    }
+    
+    @PostMapping("/insertarPorParametros")
+    public ResponseEntity<String> insertarHabilidad(@RequestParam String idHabilidad, @RequestParam String idFicha, @RequestParam String nombre, @RequestParam String competencia)
+    {
+        ResponseEntity<String> res = new ResponseEntity<>("Error insertando habilidad",HttpStatus.BAD_REQUEST);
+        Habilidad h;
+        try {
+            
+            if (!habilidadServicio.existsByIdHabilidad(idHabilidad)) {
+                
+                h = Habilidad.builder()
+                        .idHabilidad(idHabilidad)
+                        .idFicha(idFicha)
+                        .nombre(nombre)
+                        .competencia(competencia)
+                        .build();
+                
+                habilidadServicio.annadiHabilidad(h);
+                
+                res = new ResponseEntity<String>("Habilidad insertada correctamente", HttpStatus.OK);
+            }
+            
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         
         return res;
     }
