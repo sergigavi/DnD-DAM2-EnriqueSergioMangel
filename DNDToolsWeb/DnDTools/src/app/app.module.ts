1b12cf55e8bde3cfc7ab5d4222df9bd54469c558
Sergio García <98185143+sergigavi@users.noreply.github.com>
2022-11-03 14:47:39 +0100

    *

--- DNDToolsWeb/DnDTools/src/app/app.module.ts
+++ DNDToolsWeb/DnDTools/src/app/app.module.ts
@@ -1,22 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { FormsModule } from '@angular/forms'
 //import {MatSidenavModule} from '@angular/material/sidenav';
 //import {MatBadgeModule} from '@angular/material/badge';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UsuarioComponent } from './usuario/usuario.component';
+import { UsuarioFormComponent } from './usuario-form/usuario-form.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    UsuarioComponent
+    UsuarioComponent,
+    UsuarioFormComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ DNDToolsWeb/DnDTools/src/app/usuario-form/usuario-form.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { UsuarioComponent } from '../usuario/usuario.component';
+
+@Component({
+  selector: 'usuario-form',
+  templateUrl: './usuario-form.component.html',
+  styleUrls: ['./usuario-form.component.css']
+})
+export class UsuarioFormComponent implements OnInit {
+
+  usuarioModel = new UsuarioComponent();
+
+  constructor() {
+
+   }
+
+  ngOnInit(): void {
+  }
+
+  submitedFormUsuario()
+  {
+    console.log("Se ha insertado el usuario: ", this.usuarioModel)
+  }
+
+}
