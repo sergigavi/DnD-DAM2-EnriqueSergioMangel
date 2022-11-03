8ef4f22331d5fa545a5d21dfae69df6a9db61158
Sergio García <98185143+sergigavi@users.noreply.github.com>
2022-11-03 13:42:39 +0100

    delete usuarioS component

--- DNDToolsWeb/DnDTools/src/app/app.module.ts
+++ DNDToolsWeb/DnDTools/src/app/app.module.ts
@@ -3,13 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { UsuariosComponent } from './usuarios/usuarios.component';
 import { UsuarioComponent } from './usuario/usuario.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    UsuariosComponent,
     UsuarioComponent
   ],
   imports: [
