99e879f8db69c6dfe5dec0a518ba8864dd92c574
Sergio García <98185143+sergigavi@users.noreply.github.com>
2022-11-08 12:31:34 +0100

    Update app.module.ts

--- DNDToolsWeb/DnDTools/src/app/app.module.ts
+++ DNDToolsWeb/DnDTools/src/app/app.module.ts
@@ -2,15 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { FormsModule } from '@angular/forms'
-//import { HttpClientModule } from "@angular/common/http";  //  Para las peticiones http a la api
+import { HttpClientModule } from "@angular/common/http";  //  Para las peticiones http a la api
 //import {MatSidenavModule} from '@angular/material/sidenav';
 //import {MatBadgeModule} from '@angular/material/badge';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UsuarioFormComponent } from './components/usuario-form/usuario-form.component';
-//import { UsuarioServiceService } from './services/usuario-service/usuario-service.service';
-//import { Usuario } from './modelo/usuario/usuario';
+import { UsuarioServiceService } from './services/usuario-service/usuario-service.service';
+import { Usuario } from './modelo/usuario/usuario';
 
 @NgModule({
   declarations: [
@@ -20,9 +20,10 @@ import { UsuarioFormComponent } from './components/usuario-form/usuario-form.com
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule
   ],
-  //providers: [UsuarioServiceService],
+  providers: [UsuarioServiceService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
