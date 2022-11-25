46999b4b196851968d9ee7eb4d13c331ad07f8a9
Sergio García <98185143+sergigavi@users.noreply.github.com>
2022-11-25 19:27:31 +0100

    cookieservice JWT

--- DNDToolsWeb/DnDTools/src/app/app.module.ts
+++ DNDToolsWeb/DnDTools/src/app/app.module.ts
@@ -21,6 +21,8 @@ import { MatDialogModule } from '@angular/material/dialog';
 import { HttpClientModule } from "@angular/common/http";  //  Para las peticiones http a la api
 import { MatSelectModule } from '@angular/material/select';
 
+import { CookieService } from 'ngx-cookie-service';
+
 import { AppComponent } from './app.component';
 import { UsuarioFormComponent } from './components/usuario-form/usuario-form.component';
 import { UsuarioServiceService } from './services/usuario-service/usuario-service.service';
@@ -139,7 +141,7 @@ const routes: Routes = [
 
   ],
   entryComponents: [UsuarioFormComponent],
-  providers: [],
+  providers: [CookieService],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
