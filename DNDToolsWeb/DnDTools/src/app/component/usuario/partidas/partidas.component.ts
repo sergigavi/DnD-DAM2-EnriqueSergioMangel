1dba89731f5cae17746068b7d27439d4f1a7af56
Wolf <114694419+WolfHunterX@users.noreply.github.com>
2022-12-11 17:16:20 +0100

    daw
    awdw

--- DNDToolsWeb/DnDTools/src/app/component/usuario/partidas/partidas.component.ts
+++ DNDToolsWeb/DnDTools/src/app/component/usuario/partidas/partidas.component.ts
@@ -74,6 +74,10 @@ export class PartidasUsuarioComponent implements OnInit  {
     this.idCurrentUser=this.cookieService.get("CurrentUserId");
   }
 
+  goToPage(pageName:string){
+    this.router.navigate([`${pageName}`]);
+  }
+
 }
 
 @Component({
